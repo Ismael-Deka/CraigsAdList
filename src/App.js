024eed66b45e0a88bfad376cfbae60df16625fcc
Pavel Popov <pavelpopov@Pavels-MacBook-Pro.local>
2022-04-17 23:05:46 -0400

    added /ads page for ads, reserving / for future landing page

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
       <MenuBar />
       <Routes>
         <Route path="/" element={<AdsPage />} />
+        <Route path="/ads" element={<AdsPage />} />
         <Route path="/channels" element={<ChannelsPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
