665c4352e6f113b05c74344305a35e32b89690b2
Ismael-Deka <ismael.deka7899@gmail.com>
2025-06-11 09:25:25 -0400

    Allow guest users to view campaign pages.

--- src/components/ui/js/menu_bar/MenuBar.js
+++ src/components/ui/js/menu_bar/MenuBar.js
@@ -77,7 +77,7 @@ function MenuBar() {
         const isLoggedInResponse = await fetch('/is_logged_in', { method: 'GET' });
         const isLoggedInData = await isLoggedInResponse.json();
         setState((prevState) => ({ ...prevState, isLoggedIn: isLoggedInData.isuserloggedin }));
-        if (!isLoggedInData.isuserloggedin && location.pathname !== '/' && !['/login', '/signup', '/search', '/profile', '/platform'].some((path) => location.pathname.startsWith(path))) {
+        if (!isLoggedInData.isuserloggedin && location.pathname !== '/' && !['/login', '/signup', '/search', '/profile', '/platform', '/campaign'].some((path) => location.pathname.startsWith(path))) {
           setState((prevState) => ({
             ...prevState,
             errorMessage: 'Please log in.',
